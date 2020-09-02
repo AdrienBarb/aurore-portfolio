3db0dc502377a3cba6e6b10286a33df07a58f91e
Adrien Barbier <adrien-barbier@hotmail.fr>
2020-09-02 17:24:15 +0200

    Small style fixes, and add security for admin

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,5 +1,5 @@
 class Admin::ApplicationController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authorize_admin!
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -8,6 +8,13 @@ class Admin::ApplicationController < ApplicationController
   end
 
   protected
+
+  def authorize_admin!
+    authenticate_user!
+    unless current_user.admin?
+      redirect_to root_path, alert: "You must be an admin to do that."
+    end
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone_number, :title, :description])
